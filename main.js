2c8308634a7116d123694159a7eaf5aaa3e88e3e
Leah Sanchez <leahsanchez@Leahs-MacBook-Pro.local>
2022-03-09 14:32:38 -0700

    completed assignment

---
+++ main.js
@@ -0,0 +1,27 @@
+let p = document.querySelector('p');
+p.style.color = 'lightblue';
+
+p.innerText = "> Lorem ipsum dolor amet viral meh selfies drinking vinegar, intelligentsia poke flannel twee paleo enamel pin cray. Banjo celiac crucifix, kickstarter la croix air plant jianbing hashtag vinyl hell of man bun selvage schlitz banh mi. Tacos hella raclette quinoa blog, williamsburg adaptogen tbh. Hexagon af stumptown lumbersexual synth gentrify quinoa enamel pin celiac master cleanse. Truffaut typewriter shoreditch, semiotics iceland mixtape taxidermy umami distillery austin hashtag. Food truck synth wayfarers, street art banh mi actually authentic. Bitters tousled tattooed vegan neutra pug hell of fixie chia unicorn letterpress."
+
+let header = document.querySelector('h1');
+header.style.fontSize = '48px';
+
+let item13 = document.querySelector('#item-13');
+item13.style.opacity = '0.5'
+
+let item3 = document.querySelector('#item-3');
+item3.innerText = '> I say, "Hi!"'
+
+let image = document.querySelector('img');
+
+image.src = "http://www.tioxic.com/wp-content/uploads/trex_4.jpg";
+image.style.height = "300px";
+
+let image2 = document.querySelector('#red-panda');
+
+image2.src = "https://www.cbc.ca/kids/images/chinaanimals_panda.jpg";
+image2.style.height = "300px";
+
+let item16 = document.querySelector('#item-16');
+item16.class = "item"
+item16.innerText = "> Won't get fooled again."
